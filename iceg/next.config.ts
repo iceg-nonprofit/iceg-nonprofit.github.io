3879316116c38b8fe77c32f070cc3c96243fa79e
V-Paritosh <paritoshv08@gmail.com>
2025-03-15 04:24:33 -0500

    config updated

--- iceg/next.config.ts
+++ iceg/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  // basePath: "/ICEG", // Your GitHub Pages repo name
+  // assetPrefix: "/ICEG/", // Fix missing CSS/JS
+  trailingSlash: true, // Ensures proper routing
   eslint: {
     ignoreDuringBuilds: true,
   },
