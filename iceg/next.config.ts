b3f2d6b342ad1186fddaf6ced0ca8d7466967d5c
V-Paritosh <paritoshv08@gmail.com>
2025-03-15 04:17:56 -0500

    Fix deployment: include out folder

--- iceg/next.config.ts
+++ iceg/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/iceg", // Ensure this matches your repo name!
-  assetPrefix: "/iceg/", // Trailing slash is important!
   eslint: {
     ignoreDuringBuilds: true,
   },
