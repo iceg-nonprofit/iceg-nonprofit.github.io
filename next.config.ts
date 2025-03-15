4c95411aa60d3aff439b9c3e74d82d1e288977da
V-Paritosh <paritoshv08@gmail.com>
2025-03-15 05:08:41 -0500

    new rules and build files

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // basePath: "/ICEG", // Your GitHub Pages repo name
-  // assetPrefix: "/ICEG/", // Fix missing CSS/JS
-  // trailingSlash: true, // Ensures proper routing
   eslint: {
     ignoreDuringBuilds: true,
   },
